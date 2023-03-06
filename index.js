685dc2831216a23ecd1788b55f27dfb098b7e9e3
Aruna Turay <tura_arun@yahoo.com>
2023-03-06 14:59:43 -0500

    while

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+function writeCards(names, holiday) {
+  const messages = [];
+
+  for (let i = 0; i < names.length; i++ ) {
+  messages.push(`Thank you, ${names[i]}, for the wonderful ${holiday} gift!`)
+  }
+return messages;
+}
+
+function countDown() {
+ let countDown = 10;
+
+ while ( countDown >= 0) {
+  console.log(countDown--);
+ }
+}
