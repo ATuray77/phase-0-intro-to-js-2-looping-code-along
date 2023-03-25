f0283791e021dced2932f25799491a4dcc461b42
Aruna Turay <tura_arun@yahoo.com>
2023-03-25 12:15:45 -0400

    loops

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
-function writeCards(names, holiday) {
+function writeCards(names, birthday) {
   const messages = [];
 
   for (let i = 0; i < names.length; i++ ) {
-  messages.push(`Thank you, ${names[i]}, for the wonderful ${holiday} gift!`)
+  messages.push(`Thank you, ${names[i]}, for the wonderful ${birthday} gift!`)
   }
 return messages;
 }
